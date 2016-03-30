f0e3c98c45f8470937595ff2d7f2518e229dcac2
Gendai <t.brenet@orange.fr>
2016-03-30 11:19:41 +0100

    Milestone 1

---
+++ app/src/main/java/com/example/english/minesweeper/CustomView.java
@@ -0,0 +1,29 @@
+package com.example.english.minesweeper;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CustomView extends View{
+
+    public CustomView(Context c) {
+        super(c);
+        init();
+    }
+
+    public CustomView(Context c, AttributeSet attributeSet) {
+        super(c, attributeSet);
+        init();
+    }
+
+    public CustomView(Context c, AttributeSet attributeSet, int style) {
+        super(c, attributeSet, style);
+        init();
+    }
+
+    private void init()
+    {
+
+    }
+}
